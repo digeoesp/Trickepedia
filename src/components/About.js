8f22610e047858432966f51829c6822fb146ace9
digeoesp <diegocrypto@outlook.com>
2021-07-15 23:31:57 -0700

    deleted component

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <div>
+                <h1>About Me</h1>
+            </div>
+            <p> San Diego based software developer with a passion for solving complex problems in Javascript and Python. </p>
+            <p> My background in a demanding, fast-paced environment prepared me to tackle challenges efficiently.</p>
+            <p> I work well in a team environment by communicating clearly, pulling my own weight, and helping others. </p>
+        </div>
+    )
+}
+
+export default About;
